b85258800291cffa64c3533d49efe33776c7f5a9
Maicon Gomes <63738170+maiconmg@users.noreply.github.com>
2025-11-07 16:38:44 -0300

    Renda Fixa module changes and features

---
+++ aliquota.py
@@ -0,0 +1,19 @@
+tax_ir = {
+    '180': 22.5,
+    '360': 20.0,
+    '720': 17.5,
+    'above_720': 15.0
+}
+
+def get_aliquota(months: int) -> float:
+    """Returns the income tax rate according to the time period in months."""
+    days = months * 30  
+    
+    if days <= 180:
+        return tax_ir['180'] / 100
+    if days <= 360:
+        return tax_ir['360'] / 100
+    if days <= 720:
+        return tax_ir['720'] / 100
+    else:
+        return tax_ir['above_720'] / 100
--- renda_fixa.py
+++ renda_fixa.py
@@ -1,6 +1,7 @@
-from main import get_aliquota
+from aliquota import get_aliquota
 
-def renda_fixa(value: float, month_rate: float, month_period: int):
+def renda_fixa(value: float, monthly_investment: float, 
+               month_rate: float, month_period: int):
     """
     value : float : initial amount invested
     month_rate : float : monthly interest rate
@@ -8,14 +9,29 @@ def renda_fixa(value: float, month_rate: float, month_period: int):
 
     return: float: final amount after the period
     """
+    investment_value = monthly_investment * month_period
     future_value = value
-    for _ in range(month_period):
-        future_value *= (1 + month_rate / 100)
+    month_gain = 0
+    total_investiment = value + investment_value 
+    
+    for i in range(month_period):
+        future_value *= ( month_rate / 100 + 1) 
+        month_gain = (future_value - value) - (monthly_investment * i)
+        future_value += monthly_investment
+        print(f'R$ {future_value:,.2f} | gain until now: R$ {month_gain:,.2f}')
 
 
-    gross_gain = future_value - value
+    gross_gain = future_value - total_investiment
     ir_amount = gross_gain * get_aliquota(month_period)
     net_value = future_value - ir_amount
+    real_gain = net_value - total_investiment
+
+    print(f'\nYou will have invested R$ {investment_value:,.2f} for you own pocket\n')
+    print(f'You will have R$ {future_value:,.2f} gross\n')
+    print(f'You will have R$ {net_value:,.2f} net\n')
+    print(f'You will pay R$ {ir_amount:,.2f} in income tax\n')
+    print(f'You will have R$ {real_gain:,.2f} for real gain\n')
+    print(f'Income tax rate applied: {get_aliquota(month_period)*100:.2f}%\n')
     
-    print(f'You will have R$ {future_value:,.2f} gross')
-    print(f'You will have R$ {net_value:,.2f} net')
+
+teste = renda_fixa(1000, 100, 1, 12)
